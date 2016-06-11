2e5b880a7d17acafa3deea92f28db7e02a48d2e7
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-11 11:48:23 +0200

    add cluster create and destroy

---
+++ scripts/cluster-create.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
+
+echo "downloading image ..."
+wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+#wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+#wget http://alpha.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+
+echo "converting image ..."
+qemu-img convert /var/lib/libvirt/images/coreos_production_qemu_image.img -O raw /var/lib/libvirt/images/coreos_production_qemu_image.raw
+
+echo "create lvm volumes ..."
+
+lvcreate -L 10G -n kubernetes system
+lvcreate -L 10G -n kubernetes-docker system
+
+
+echo "writing images ..."
+
+dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/system/kubernetes
+
+
+echo "cleanup"
+rm /var/lib/libvirt/images/coreos_production_qemu_image.img /var/lib/libvirt/images/coreos_production_qemu_image.raw
+
+${SCRIPT_ROOT}/virsh-create.sh
+
+echo "done"
---
+++ scripts/cluster-destroy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
+
+${SCRIPT_ROOT}/virsh-destroy.sh
+${SCRIPT_ROOT}/virsh-undefine.sh
+
+echo "remove lvm volumes ..."
+
+lvremove /dev/system/kubernetes
+lvremove /dev/system/kubernetes-docker
+
+
+echo "done"
