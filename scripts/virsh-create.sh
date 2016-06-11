89ffaaa7067de3c95cf156b8e9c495f61b5a0fcb
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-11 13:46:31 +0200

    add create virsh

---
+++ scripts/virsh-create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+
+echo "create virsh kubernetes mac=00:16:3e:2f:90:0b ..."
+virt-install \
+--import \
+--debug \
+--serial pty \
+--accelerate \
+--ram 2000 \
+--vcpus 4 \
+--cpu=host \
+--os-type linux \
+--os-variant virtio26 \
+--noautoconsole \
+--nographics \
+--name kubernetes \
+--disk /dev/system/kubernetes,bus=virtio,cache=none,io=native \
+--disk /dev/system/kubernetes-docker,bus=virtio,cache=none,io=native \
+--disk /dev/system/kubernetes-data,bus=virtio,cache=none,io=native \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-master/config/,config-2,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-master/ssl/,kubernetes-ssl,type=mount,mode=squash \
+--network bridge=br0,mac=00:16:3e:2f:90:0b,model=virtio
+
