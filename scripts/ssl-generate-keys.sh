29cbd1ca4b3f34c75fd97bedbbbba6e182cca253
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-11 18:52:34 +0200

    move admin down

--- scripts/ssl-generate-keys.sh
+++ scripts/ssl-generate-keys.sh
@@ -13,15 +13,17 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 openssl genrsa -out ${SCRIPT_ROOT}/ca-key.pem 2048
 openssl req -x509 -new -nodes -key ${SCRIPT_ROOT}/ca-key.pem -days 10000 -out ${SCRIPT_ROOT}/ca.pem -subj "/CN=kube-ca"
 
+
+# kubernetes
+openssl genrsa -out ${SCRIPT_ROOT}/kubernetes-key.pem 2048
+KUBERNETES_SVC=10.103.0.1 FIREWALL_IP=172.16.70.4 MASTER_IP=172.16.90.10 openssl req -new -key ${SCRIPT_ROOT}/kubernetes-key.pem -out ${SCRIPT_ROOT}/kubernetes.csr -subj "/CN=kubernetes" -config ${SCRIPT_ROOT}/master-openssl.cnf
+KUBERNETES_SVC=10.103.0.1 FIREWALL_IP=172.16.70.4 MASTER_IP=172.16.90.10 openssl x509 -req -in ${SCRIPT_ROOT}/kubernetes.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/kubernetes.pem -days 365 -extensions v3_req -extfile ${SCRIPT_ROOT}/master-openssl.cnf
+
+
+
+
 # Admin Key
 openssl genrsa -out ${SCRIPT_ROOT}/admin-key.pem 2048
 openssl req -new -key ${SCRIPT_ROOT}/admin-key.pem -out ${SCRIPT_ROOT}/admin.csr -subj "/CN=kube-admin"
 openssl x509 -req -in ${SCRIPT_ROOT}/admin.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/admin.pem -days 365
-
-
-# kubernetes
-openssl genrsa -out ${SCRIPT_ROOT}/kubernetes-key.pem 2048
-KUBERNETES_SVC=10.103.0.1 FIREWALL_IP=172.16.70.4 MASTER_IP=172.16.90.10 openssl req -new -key ${SCRIPT_ROOT}/kubernetes-key.pem -out ${SCRIPT_ROOT}/kubernetes.csr -subj "/CN=kube-apiserver" -config ${SCRIPT_ROOT}/master-openssl.cnf
-KUBERNETES_SVC=10.103.0.1 FIREWALL_IP=172.16.70.4 MASTER_IP=172.16.90.10 openssl x509 -req -in ${SCRIPT_ROOT}/kubernetes.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/kubernetes.pem -days 365 -extensions v3_req -extfile ${SCRIPT_ROOT}/master-openssl.cnf
-
 
