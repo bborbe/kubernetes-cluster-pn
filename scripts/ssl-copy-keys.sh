9d1d68a1f4a417f08ea3fc274e48d028a471446a
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-11 11:56:45 +0200

    add copy ssl

---
+++ scripts/ssl-copy-keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
+
+
+mkdir -p ${SCRIPT_ROOT}/../kubernetes/ssl
+cp ${SCRIPT_ROOT}/kubernetes-ca.pem ${SCRIPT_ROOT}/../kubernetes/ssl/ca.pem
+cp ${SCRIPT_ROOT}/kubernetes.pem ${SCRIPT_ROOT}/../kubernetes/ssl/node.pem
+cp ${SCRIPT_ROOT}/kubernetes-key.pem ${SCRIPT_ROOT}/../kubernetes/ssl/node-key.pem
+#chmod 600 ${SCRIPT_ROOT}/../kubernetes/ssl/*.pem
+chown root:root ${SCRIPT_ROOT}/../kubernetes/ssl/*.pem
+
