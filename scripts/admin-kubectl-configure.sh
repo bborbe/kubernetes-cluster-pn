6295561391976efa3a0ebc327076fdef9c6dbd0f
Benjamin Borbe <bborbe@rocketnews.de>
2016-10-16 12:27:57 +0200

    update kubernetes network

--- scripts/admin-kubectl-configure.sh
+++ scripts/admin-kubectl-configure.sh
@@ -8,7 +8,7 @@ set -o errtrace
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 
 mkdir -p $HOME/.kube/pn
-kubectl config set-cluster pn-cluster --server=https://172.16.90.10:443 --certificate-authority=$HOME/.kube/pn/ca.pem
+kubectl config set-cluster pn-cluster --server=https://172.16.72.10:443 --certificate-authority=$HOME/.kube/pn/ca.pem
 kubectl config set-credentials pn-admin --certificate-authority=$HOME/.kube/pn/ca.pem --client-key=$HOME/.kube/pn/admin-key.pem --client-certificate=$HOME/.kube/pn/admin.pem
 kubectl config set-context pn-system --cluster=pn-cluster --user=pn-admin
 kubectl config use-context pn-system
